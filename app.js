0788e38d9a763012dbe5ea247fb3b1bec7be3467
Ana Gabriela Gomez <anitgomezok@gmail.com>
2025-05-17 03:45:12 -0300

    Modelo de usuario creado con encriptación de contraseña y ruta de prueba

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();                  //Creamos una instancia de la app
 const path = require("path");            //Importamos el módulo path
 const mongoose = require("mongoose");  //Importamos Mongoose
 
+const User = require("./models/User"); //Importamos el modelo de usuario
+
 app.set("view engine", "ejs");           //Configuramos el motor de plantillas EJS
 app.set("views", path.join(__dirname, "views")); //Configuramos la carpeta de vistas
 
@@ -15,9 +17,18 @@ async function main() {
 }
 
 
-app.use("/", (req, res) => {            //Definímos una ruta raíz "/"
-    res.render("home.ejs") //Renderizamos la vista home.ejs
-});
+app.get("/", async (req, res) => { //Definimos la ruta raíz
+    let user= new User({ //Creamos un nuevo usuario
+        name: "Ana Gomez", //Nombre del usuario
+        email: "anitgomezok@gmail.com", //Email del usuario
+        password: "123456", //Contraseña del usuario
+        role: "admin" //Rol del usuario
+    });
+
+    let admin = await user.save(); //Guardamos el usuario en la base de datos
+    res.send(admin) //Enviamos el usuario como respuesta
+
+})
 
 app.listen(8080, () => {                //El servidor escucha en el puerto 8080
     console.log("Server is running on port 8080");
