62b2638381299cd73df6282a715a9627cf3990c8
moyeta-tech <octaviomoya05@gmail.com>
2025-05-18 16:22:45 -0300

    archivo Productos agregado

---
+++ models/Productos.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+    nombre: { type: String, required: true },
+    descripcion: String,
+    precio: { type: Number, required: true },
+    stock: { type: Number, required: true },
+    categoria: { type: String, required: true }
+});
+
+module.exports = mongoose.model('Product', productSchema);
